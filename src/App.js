eb9486a8e4c6fa74287851070c85a11223cc3c64
Chetali1999 <chetalipardhi14@gmail.com>
2024-04-10 21:34:15 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ClockHeading from './Components/ClockHeading';
+import ClockSlogan from './Components/ClockSlogan';
+import CurrentTime from './Components/CurrentTime';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <ClockHeading />
+      <ClockSlogan />
+      <CurrentTime />
+    </center>
   );
 }
 
---
+++ src/Components/ClockSlogan.js
@@ -0,0 +1,12 @@
+
+
+const ClockSlogan = () => {
+
+    return (
+        <>
+        <p className='lead'>This is the clock that shows the time in bharat at all time.</p>
+        </>
+    )
+}
+
+export default ClockSlogan;
